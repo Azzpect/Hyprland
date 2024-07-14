1089ebdbef731c18697fa5e24f69197896c69344
Azzpect <atanughosh.brp@gmail.com>
2024-07-14 19:39:51 +0530

    added tmux

--- setup.sh
+++ setup.sh
@@ -10,15 +10,22 @@ echo "updating aur"
 sudo pacman -Syu
 
 echo "downloading hyprland, sddm, dolphing waybar, alcritty, neovim, pipewire, wireplumber, pipewire-pulse, pavucontrol" 
-yay -S hyprland-git sddm firefox dolphin waybar alacritty neovim pipewire wireplumber pipewire-pulse pavucontrol tmux
+yay -S hyprland-git sddm firefox dolphin waybar alacritty neovim pipewire wireplumber pipewire-pulse pavucontrol stow neofetch
 
 #setting up zsh
 yay -S zsh
+echo "installing oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "cloning powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "cloning zsh autosuggestiong"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 rm ~/.zshrc
+
+
+#setting up tmux
+yay -S tmux
+echo "installing tpm"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #move the .xinitrc file

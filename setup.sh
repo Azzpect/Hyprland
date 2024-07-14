1eab32e6ae4db7b93430a4146b9c1cc4cf0c3c0c
Azzpect <atanughosh.brp@gmail.com>
2024-07-14 19:13:08 +0530

    added waybar

--- setup.sh
+++ setup.sh
@@ -1,8 +1,24 @@
+echo "Cloning yay repository to ~/.yay"
 git clone https://aur.archlinux.org/yay.git ~/.yay
 
 cd ~/.yay
+echo "building yay from source"
 makepkg -si
+
+
+echo "updating aur"
 sudo pacman -Syu
-yay -S hyprland-git sddm firefox dolphin waybar alacritty neovim pipewire wireplumber pipewire-pulse pavucontrol
+
+echo "downloading hyprland, sddm, dolphing waybar, alcritty, neovim, pipewire, wireplumber, pipewire-pulse, pavucontrol" 
+yay -S hyprland-git sddm firefox dolphin waybar alacritty neovim pipewire wireplumber pipewire-pulse pavucontrol tmux
+
+#setting up zsh
+yay -S zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "cloning powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "cloning zsh autosuggestiong"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm ~/.zshrc
 
 #move the .xinitrc file

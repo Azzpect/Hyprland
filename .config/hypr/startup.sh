9ba7ffcea71bc326274b4bc929d483b3b4dcbc34
Azzpect <atanughosh.brp@gmail.com>
2024-07-17 22:51:48 +0530

    wallpaper engines are set

--- .config/hypr/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sleep 5;
-wal -i "/home/azzpect/.config/wallpapers/3.jpg"
-hyprctl hyprpaper preload "/home/azzpect/.config/wallpapers/3.jpg" && hyprctl hyprpaper wallpaper ",/home/azzpect/.config/wallpapers/3.jpg"
--- .config/wallpapers/setWal.sh
+++ .config/wallpapers/setWal.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 file_path=$(zenity --file-selection --title="Select a wallpaper" --filename="~/.config/wallpapers/")
+hypr=~/.config/hypr/hyprpaper.conf
 
+echo "unloading wallpapers"
 hyprctl hyprpaper unload all
+echo "generating color schemas"
 wal -i "$file_path"
+# Clear previous wallpaper settings
+sed -i '/preload =/d' "$hypr"
+sed -i '/wallpaper =/d' "$hypr"
+
+# Update hyprpaper.conf
+echo "preload = $file_path" >> "$hypr"
+echo "wallpaper = ,$file_path" >> "$hypr"
+
+#setting the wallpaper
+echo "preloading wallpaper"
 hyprctl hyprpaper preload "$file_path"
+echo "setting wallpaper"
 hyprctl hyprpaper wallpaper ",$file_path"
 
-printf "#!/bin/bash\nsleep 5;\nwal -i \"$file_path\"\nhyprctl hyprpaper preload \"$file_path\" && hyprctl hyprpaper wallpaper \",$file_path\"" > ~/.config/hypr/startup.sh
-
-
-
+echo "copying rofi color schemes"
 if [ ! -e "~/.config/rofi/colors.rasi" ]; then
   touch ~/.config/rofi/colors.rasi
 fi
 
+echo "generating rofi color template"
 printf "@import \"~/.cache/wal/colors-rofi-light\"\nelement-text {\nbackground-color: inherit;\ntext-color: inherit;\n}" > ~/.config/rofi/colors.rasi
 
-
+hyprctl reload

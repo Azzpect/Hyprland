13a14b4d441490e2753bd4b99b602de1bb5bdf85
Azzpect <atanughosh.brp@gmail.com>
2025-10-20 16:55:55 +0530

    added dunst as notification manager

--- .config/wallpapers/setWal.sh
+++ .config/wallpapers/setWal.sh
@@ -21,6 +21,22 @@ fi
 echo "copying color schemas"
 cp ~/.cache/wal/colors.scss ~/.config/eww/
 cp ~/.cache/wal/colors-kitty.conf ~/.config/kitty/
+cp ~/.cache/wal/colors.json ~/.config/dunst/
+
+echo "generating dunstrc"
+background=$(cat ~/.config/dunst/colors.json | jq -r '.special.background')
+foreground=$(cat ~/.config/dunst/colors.json | jq -r '.special.foreground')
+color=$(cat ~/.config/dunst/colors.json | jq -r '.colors.color1')
+low="[urgency_low]\n\tbackground = \"$background\"\n\tforeground = \"$foreground\"\n\tframe_color = \"$foreground\"\n\ttimeout = 10"
+normal="[urgency_normal]\n\tbackground = \"$background\"\n\tforeground = \"$foreground\"\n\tframe_color = \"$foreground\"\n\thighlight = \"$color\"\n\ttimeout = 10"
+critical="[urgency_critical]\n\tbackground = \"$background\"\n\tforeground = \"$foreground\"\n\tframe_color = \"$color\"\n\ttimeout = 0"
+
+cat ~/.config/dunst/dunstrc.template > ~/.config/dunst/dunstrc
+echo -e "\n$low\n\n$normal\n\n$critical" >> ~/.config/dunst/dunstrc
+
+echo "restarting dunst"
+killall dunst
+dunst &
 
 echo "generating rofi color template"
 printf "@import \"~/.cache/wal/colors-rofi-dark.rasi\"\n@import \"~/.config/rofi/rofi.rasi\"\nelement-text {\nbackground-color: inherit;\ntext-color: inherit;\n}" > ~/.config/rofi/colors.rasi

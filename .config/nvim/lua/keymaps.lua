73942db9eec311f8fd9794b37884175d3eddd8aa
Azzpect <atanughosh.brp@gmail.com>
2026-01-09 22:56:15 +0530

    added error message system and updated treesitter syntax

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -18,3 +18,8 @@ vim.keymap.set('n', '<leader>j', '<C-w>j', {})
 vim.keymap.set('n', '<leader>h', '<C-w>h', {})
 vim.keymap.set('n', '<leader>k', '<C-w>k', {})
 vim.keymap.set('n', '<leader>l', '<C-w>l', {})
+
+
+vim.diagnostic.config({ 
+  virtual_text = false, virtual_lines = { current_line = true }
+}) 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,24 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   build = ':TSUpdate',
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        'bash',
+        'c',
+        'html',
+        'lua',
+        'markdown',
+        'vim',
+        'python',
+        'javascript',
+        'json',
+        'typescript',
+        'css',
+        'java',
+        'go'
+      },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
